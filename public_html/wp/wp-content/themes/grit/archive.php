2dc78483ecc34f1ff93a9266b0bcd8a8d4b8748c
ICHIMURA\icimu <ichimuranora@gmail.com>
2023-02-04 17:28:01 +0900

    お知らせ一覧ページwp組み込み

--- public_html/wp/wp-content/themes/grit/archive.php
+++ public_html/wp/wp-content/themes/grit/archive.php
@@ -13,19 +13,19 @@
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
   <meta name="format-detection" content="telephone=no">
-  <title>grit</title>
+  <title>news｜grit</title>
   <script src="/inc/js/page/viewport-extra.min.js"></script>
   <script>new ViewportExtra(375);</script>
   <link rel="icon" href="/inc/image/favicon.ico" type="image/x-icon">
-  <meta name="description" content="gritの「」についてのページです。">
+  <meta name="description" content="gritの「news」についてのページです。">
   <meta property="og:site_name" content="grit" />
-  <meta property="og:title" content="grit" />
-  <meta property="og:description" content="gritの「」についてのページです。" />
+  <meta property="og:title" content="news｜grit" />
+  <meta property="og:description" content="gritの「news」についてのページです。" />
   <meta property="og:type" content="article" />
-  <meta property="og:url" content="">
+  <meta property="og:url" content="/news/">
   <meta property="og:image" content="/inc/image/common/img_ogp.jpg" />
   <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="">
+  <meta name="twitter:site" content="/news/">
   <meta name="twitter:image" content="/inc/image/common/img_ogp.jpg">
   <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,500,700&display=swap&subset=japanese" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="/inc/css/style.css">
@@ -33,7 +33,7 @@
   <link rel="stylesheet" type="text/css" href="/inc/css/page/archive.css">
 </head>
 
-<body id="">
+<body id="archive-news">
   <div id="container">
     <header class="l-header js-header">
       <div class="l-header__inner">
@@ -60,7 +60,7 @@
               <a href="/faq/"><span class="l-navigation__ja">よくある質問</span>FAQ</a>
             </li>
             <li class="l-navigation__item">
-              <a href="/news/"><span class="l-navigation__ja">お知らせ</span>NEWS</a>
+              <a class="current" href="/news/"><span class="l-navigation__ja">お知らせ</span>NEWS</a>
             </li>
             <li class="l-nav">
               <ul class="l-nav__btns">
@@ -80,16 +80,59 @@
         <div class="l-lowerCaption__inner">
           <div class="l-lowerCaption__titleBox">
             <h1 class="l-lowerCaption__title">
-              <small></small>
-            </h1>
+              <small>news</small> お知らせ </h1>
           </div>
           <ul class="c-topicspath">
             <li><a href="/">HOME</a></li>
-            <li></li>
+            <li>news</li>
           </ul>
         </div>
       </div>
       <div id="contents" class="l-lower">
+        <div class="l-section l-section--sm">
+          <ul class="c-newslist">
+            <?php
+        $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;//ポイント1
+        $args = array(
+          'paged' => $paged,//ポイント2
+          'posts_per_page' => 6,//ポイント3
+          );
+          $query = new WP_Query( $args );
+          ?>
+            <?php if( $query->have_posts() ) : ?>
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <li class="c-newslist__item">
+              <a href="<?php the_permalink(); ?>">
+                <dl>
+                  <dt>
+                    <span class="c-newslist__date">
+                      <?php echo get_the_date("Y.m.d"); ?></span>
+                    <span class="c-label ">
+                      <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?></span>
+                  </dt>
+                  <dd>
+                    <?php the_title(); ?>
+                  </dd>
+                </dl>
+              </a>
+            </li>
+            <?php endwhile; wp_reset_postdata(); ?>
+            <?php else : ?>
+            <p class="">記事が見つかりませんでした。</p>
+            <?php endif; ?>
+          </ul>
+        </div>
+        <?php
+          $big = 999999999; // need an unlikely integer
+          echo paginate_links([
+            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+            'format' => '?page=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total' => $query->max_num_pages,
+            'prev_text' => __('&lt;'),
+            'next_text' => __('&gt;'),
+          ]);
+        ?>
         <?php if( function_exists("the_pagination") ) the_pagination(); ?>
       </div>
     </div>
