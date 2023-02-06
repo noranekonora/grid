7ef29f2326f634bef5b29a6de425619dbc9efa5f
ICHIMURA\icimu <ichimuranora@gmail.com>
2023-02-06 14:30:36 +0900

    functions.php　いらない記述削除

--- public_html/wp/wp-content/themes/grit/functions.php
+++ public_html/wp/wp-content/themes/grit/functions.php
@@ -18,89 +18,42 @@ function my_setup() {
 }
 add_action( 'after_setup_theme', 'my_setup' );
 
-// 管理画面「投稿」の名前を変更
-function Change_menulabel() {
-  global $menu;
-  global $submenu;
-  $name = '投稿';
-  $menu[5][0] = $name;
-  $submenu['edit.php'][5][0] = $name.'一覧';
-  $submenu['edit.php'][10][0] = '新しい'.$name;
-  }
-  function Change_objectlabel() {
-  global $wp_post_types;
-  $name = 'test';
-  $labels = &$wp_post_types['post']->labels;
-  $labels->name = $name;
-  $labels->singular_name = $name;
-  $labels->add_new = _x('追加', $name);
-  $labels->add_new_item = $name.'の新規追加';
-  $labels->edit_item = $name.'の編集';
-  $labels->new_item = '新規'.$name;
-  $labels->view_item = $name.'を表示';
-  $labels->search_items = $name.'を検索';
-  $labels->not_found = $name.'が見つかりませんでした';
-  $labels->not_found_in_trash = 'ゴミ箱に'.$name.'は見つかりませんでした';
-  }
-  add_action( 'init', 'Change_objectlabel' );
-  add_action( 'admin_menu', 'Change_menulabel' );
-
-// カスタム投稿タイプの追加
-// add_action( "init", "create_post_type" );
-// 	function create_post_type() {
-//     register_post_type(
-//       'schedule',
-//       array(
-//         'label' => 'リーグ戦スケジュール',
-//         'public' => true,
-//         'has_archive' => true,
-//         'show_in_rest' => true,
-//         'menu_position' => 5,
-//         'supports' => array(
-//           'title',
-//           'editor',
-//           'thumbnail',
-//           'revisions',
-//         ),
-//       )
-//     );
-// 	}
-
-  //カスタム投稿タイプの追加
+//カスタム投稿タイプの追加
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
 //カスタム投稿タイプ１（ここから）
 register_post_type(
-'schedule',
-array(
-'labels' => array(
-'name' => __( 'リーグ戦スケジュール1' ),
-'singular_name' => __( 'リーグ戦スケジュール1' )
-),
-'public' => true,
-'menu_position' =>5,
-)
-);
-//カスタム投稿タイプ１（ここまで）
-
-//カスタム投稿タイプ２（ここから）
-register_post_type(
   'news',
   array(
   'labels' => array(
-  'name' => __( 'news' ),
-  'singular_name' => __( 'news' )
+  'name' => __( 'お知らせ' ),
+  'singular_name' => __( 'お知らせ' )
   ),
   'public' => true,
   'menu_position' =>5,
   'has_archive' => true,
   )
   );
+//カスタム投稿タイプ１（ここまで）
+
+//カスタム投稿タイプ２（ここから）
+register_post_type(
+  'schedule',
+  array(
+  'labels' => array(
+  'name' => __( 'リーグ戦スケジュール' ),
+  'singular_name' => __( 'リーグ戦スケジュール' )
+  ),
+  'public' => true,
+  'menu_position' =>5,
+  )
+  );
 //カスタム投稿タイプ２（ここまで）
-//カテゴリーの追加
-register_taxonomy_for_object_type('category', 'news');
-//タグの追加
-register_taxonomy_for_object_type('post_tag', 'news');
+
+  //カテゴリーの追加
+  register_taxonomy_for_object_type('category', 'news');
+  //タグの追加
+  register_taxonomy_for_object_type('post_tag', 'news');
 
 }
 
